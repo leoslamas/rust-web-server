4ee8d21d2b8ac02741a594941b5bbe46166007c1
Leonardo Lamas <leoslamas@gmail.com>
2020-10-12 01:10:28 -0300

    implementing unfinished thread pool

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,16 @@ use std::fs;
 use std::thread;
 use std::time::Duration;
 
+use web_server::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
